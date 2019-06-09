4661c235f7a6d09b6a0314981beed8f3ddbec457
Manuel Glynias <manuel@hylomorphicsolutions.com>
2019-06-09 11:15:58 -0400

    Update clinvar.sh
    removed sudo

--- clinvar.sh
+++ clinvar.sh
@@ -3,7 +3,7 @@ echo 'hello from clinvar.sh'
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 echo 'calling mongod'
-sudo mongod
+mongod
 echo 'back from mongod'
 echo 'calling clinvar.py'
 python3 clinvar.py
