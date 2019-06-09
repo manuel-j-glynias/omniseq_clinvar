3500b776129ab3161498362c32925be9e3d3207f
Manuel Glynias <manuel@hylomorphicsolutions.com>
2019-06-09 11:03:50 -0400

    Update clinvar.sh
    calling mongod from shell script

--- clinvar.sh
+++ clinvar.sh
@@ -2,6 +2,9 @@
 echo 'hello from clinvar.sh'
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
+echo 'calling mongod'
+sudo mongod
+echo 'back from mongod'
 echo 'calling clinvar.py'
 python3 clinvar.py
 echo 'back from clinvar.py'
